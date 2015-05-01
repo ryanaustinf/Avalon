4d9a9ef28de1c5c02eff490250d07b7197fbc462
Austin Fernandez <ryanaustinf@yahoo.com>
2015-05-02 00:48:12 +0800

    Added Intro File Templates and Began Chap 4 SRS

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE PHP>
+<html>
+	<head>
+		<title>Avalon Online</title>
+		<link rel="stylesheet" href="external.css" />
+		<script src="jquery-2.1.1.js"></script>
+	</head>
+	<body>
+		<header>
+		</header>
